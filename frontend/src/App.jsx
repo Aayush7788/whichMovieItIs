090a1162b9c1db4e2a18e012d567ddf84905181e
Aayush7788 <aayushkotadia1234@outlook.com>
2026-06-01 11:17:19 +0530

    make search input controlable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import "./App.css"
 
 function App(){
+
+  const [query, setQuery] = useState("");
+
+  function handleSearch(event) {
+    event.preventDefault();
+    console.log(query);
+  }
+
   return(
     <main className="app-shell">
       <section>
@@ -10,8 +19,12 @@ function App(){
           Search by rough plot, memory, or partial movie description.
         </p>
 
-        <form className="search-form">
-          <input type="text" placeholder="Example: Spaceship enter in wormhole." />
+        <form className="search-form" onSubmit={handleSearch}>
+          <input type="text" 
+            value={query} 
+            onChange={(event) => setQuery(event.target.value)} 
+            placeholder="Example: Spaceship enter in wormhole." 
+          />
           <button type="submit">Search</button>
         </form>
       </section>
